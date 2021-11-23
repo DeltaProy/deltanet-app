bae0703f7acce28b0292538c915588d919db8303
Jorge Clemente <jclementeh@gmail.com>
2021-11-22 21:29:01 -0500

    Creamos el modulo Shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-
-//Angular Material
-import { MatSliderModule } from '@angular/material/slider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SharedModule } from './components/shared/shared.module';
 
 // Componentes
 import { LoginComponent } from './components/login/login.component';
@@ -28,12 +21,7 @@ import { LoginComponent } from './components/login/login.component';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    MatProgressSpinnerModule
+    SharedModule
   ],
   providers: [{provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent]
